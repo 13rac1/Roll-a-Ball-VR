42b4f9c4bf5d017b5c55d6c7828ff2152cc8286e
Brad Erickson <eosrei@gmail.com>
2017-01-15 16:24:54 -0800

    player: Camera follows player
    End of Unity 5 - Roll a Ball game - 3 of 8: Moving the Camera - Unity
    Official Tutorials: https://www.youtube.com/watch?v=Xcm5H2J95iI

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+	void Start () {
+        offset = transform.position - player.transform.position;
+	}
+	
+	// LateUpdate is called once per frame after Update() has completed
+	void Update () {
+        transform.position = player.transform.position + offset;
+	}
+}
